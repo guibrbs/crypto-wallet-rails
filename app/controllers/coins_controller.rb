58c6f9cfbf2826de575d403018a2f3df51c8a21c
guibrbs <200037994@aluno.unb.br>
2022-10-18 17:48:39 -0300

    feat: add mining type option to edit and create view

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,6 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: [:new, :create, :edit, :update]
 
   # GET /coins or /coins.json
   def index
@@ -65,6 +66,10 @@ class CoinsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def coin_params
-      params.require(:coin).permit(:description, :acronym, :url_image)
+      params.require(:coin).permit(:description, :acronym, :url_image, :mining_type_id)
+    end
+
+    def set_mining_type_option
+      @mining_type_options = MiningType.all.pluck(:description, :id)
     end
 end

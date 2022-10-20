d265f71939ac99fef18b06de821c844557a04247
guibrbs <200037994@aluno.unb.br>
2022-10-19 22:54:06 -0300

    fix: fix undefined method

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,6 +1,6 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: %i[ show edit update destroy ]
-  before_action :set_mining_type_option, only: [:new, :create, :edit, :update]
+  before_action :set_mining_type_options, only: [:new, :create, :edit, :update]
 
   # GET /coins or /coins.json
   def index
@@ -59,6 +59,9 @@ class CoinsController < ApplicationController
   end
 
   private
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:description, :id)
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])

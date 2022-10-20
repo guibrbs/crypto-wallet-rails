01902868a86393ddb396586327fb81e3e550d37f
guibrbs <200037994@aluno.unb.br>
2022-10-19 21:51:12 -0300

    build: add yarn, bootstrap, jquery and popper

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require jquery
+//= require bootstrap/dist/js/bootstrap
+//= require popper.js/dist/popper
